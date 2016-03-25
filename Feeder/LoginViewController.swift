647031dcde0490a98b01df948670097917581892
Jason Andrae <jandrae@unca.edu>
2016-03-25 16:35:51 -0400

    Added more setup for viewDidLoad, started to create logic for changing the view depending on login/create state

--- Feeder/LoginViewController.swift
+++ Feeder/LoginViewController.swift
@@ -26,10 +26,16 @@ class LoginViewController: UIViewController, UITextFieldDelegate, UIAlertViewDel
     let createLoginButtonTag = 0
     let loginButtonTag = 1
     
+    // Keychain wrapper
+    let MyKeychainWrapper = KeychainWrapper()
+    
     // Variables
     var login: Login?
     var givenUser: String?
     var givenPass: String?
+    var givenConfirm: String?
+    var givenURL: String?
+    var isAuthenticated = false
     
     override func viewDidLoad() {
         
@@ -38,11 +44,14 @@ class LoginViewController: UIViewController, UITextFieldDelegate, UIAlertViewDel
         // Load account from keychain
         let hasAccount = NSUserDefaults.standardUserDefaults().boolForKey("hasAccountKey")
         
-        // If account exists, load username from account, hide account creation label
+        // If account exists, load username from account, hide account creation label & text fields, set authenticated to true
         if hasAccount {
             loginButton.setTitle("Login", forState: UIControlState.Normal)
             loginButton.tag = loginButtonTag
             createInfoLabel.hidden = true
+            confirmTextField.hidden = true
+            urlTextField.hidden = true
+            isAuthenticated = true
         } else { // Else, change button to create account, unhide account creation label
             loginButton.setTitle("Create", forState: UIControlState.Normal)
             loginButton.tag = createLoginButtonTag
@@ -57,21 +66,33 @@ class LoginViewController: UIViewController, UITextFieldDelegate, UIAlertViewDel
         // Set up view controller to be own text field delegates
         nameTextField.delegate = self
         passTextField.delegate = self
+        confirmTextField.delegate = self
+        urlTextField.delegate = self
         
     }
     
     // MARK: UITextFieldDelegate
+    /**
+     * Function for what happens when enter key is used for a given text field
+     */
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder();
         switch textField {
         case nameTextField:
             passTextField.becomeFirstResponder()
+        case passTextField:
+            if !isAuthenticated { // If no authentication, user is creating account so go to next
+                
+            }
         default:
             print("something has gone wrong")
         }
         return true
     }
     
+    /**
+     * Function for what happens when text field entry has completed
+     */
     func textFieldDidEndEditing(textField: UITextField) {
         switch textField {
             case nameTextField:
@@ -87,6 +108,55 @@ class LoginViewController: UIViewController, UITextFieldDelegate, UIAlertViewDel
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func loginAction(sender: UIButton) {
+        // if either text field is empty, warn user with alert
+        if (nameTextField.text == "" || passTextField.text == "") {
+            let alertView = UIAlertController(title: "Login Problem",
+                                              message: "Wrong username or password." as String, preferredStyle:.Alert)
+            let okAction = UIAlertAction(title: "I got this", style: .Default, handler: nil)
+            alertView.addAction(okAction)
+            self.presentViewController(alertView, animated: true, completion: nil)
+            return;
+        }
+        
+        // Remove keyboards from text fields
+        nameTextField.resignFirstResponder()
+        passTextField.resignFirstResponder()
+        
+        // 3.
+        if sender.tag == createLoginButtonTag {
+            
+            // 4.
+            let hasLoginKey = NSUserDefaults.standardUserDefaults().boolForKey("hasLoginKey")
+            if hasLoginKey == false {
+                NSUserDefaults.standardUserDefaults().setValue(self.nameTextField.text, forKey: "username")
+            }
+            
+            // 5.
+            MyKeychainWrapper.mySetObject(passTextField.text, forKey:kSecValueData)
+            MyKeychainWrapper.writeToKeychain()
+            NSUserDefaults.standardUserDefaults().setBool(true, forKey: "hasLoginKey")
+            NSUserDefaults.standardUserDefaults().synchronize()
+            loginButton.tag = loginButtonTag
+            
+            performSegueWithIdentifier("dismissLogin", sender: self)
+        } else if sender.tag == loginButtonTag {
+            // 6.
+            if checkLogin(nameTextField.text!, password: passTextField.text!) {
+                performSegueWithIdentifier("dismissLogin", sender: self)
+            } else {
+                // 7.
+                let alertView = UIAlertController(title: "Login Problem",
+                                                  message: "Wrong username or password." as String, preferredStyle:.Alert)
+                let okAction = UIAlertAction(title: "Foiled Again!", style: .Default, handler: nil)
+                alertView.addAction(okAction)
+                self.presentViewController(alertView, animated: true, completion: nil)
+            }
+        }
     }
     
     // MARK: Navigation
@@ -105,9 +175,15 @@ class LoginViewController: UIViewController, UITextFieldDelegate, UIAlertViewDel
         
     }
 
-    // MARK: NSCoding
-    func loadLogin() -> Login? {
-        return NSKeyedUnarchiver.unarchiveObjectWithFile(Login.ArchiveURL.path!) as? Login
+    // MARK: Helper functions
+    func checkLogin(username: String, password: String ) -> Bool {
+        // Compare entered username and passwords to keychain user and pass
+        if password == MyKeychainWrapper.myObjectForKey("v_Data") as? String &&
+            username == NSUserDefaults.standardUserDefaults().valueForKey("username") as? String {
+            return true
+        } else {
+            return false
+        }
     }
 }
 
